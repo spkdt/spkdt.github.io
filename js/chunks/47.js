679b7869eab1eab44ed5407299663f16da5394aa
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-06-16 14:04:45 +0200

    second commit

---
+++ js/chunks/47.js
@@ -0,0 +1,1 @@
+webpackJsonp([47],{332:function(module,exports,__webpack_require__){try{(function(){"use strict";function removeProps(obj,props){var objRemovedProps=Object.assign({},obj);return props.forEach(function(prop){delete objRemovedProps[prop]}),objRemovedProps}Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=removeProps}).call(this)}finally{}}});
